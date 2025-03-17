5822402547024a30fec5e6b4e4fbbbab0437c755
Luis Gonzalo Morelli <gonzy.morelli@gmail.com>
2025-03-17 19:46:18 -0300

    Filtrar superhéroes mayores de 30 años que provienen de la Tierra y tienen al menos dos poderes

--- src/repositories/SuperHeroRepository.mjs
+++ src/repositories/SuperHeroRepository.mjs
@@ -15,7 +15,11 @@ class SuperHeroRepository extends IRepository {
     }
 
     async obtenerMayoresDe30() {
-        return await SuperHero.find({ edad: { $gt: 30 } });
+        return await SuperHero.find({
+            edad: { $gt: 30 },
+            planetaOrigen: 'Tierra',
+            $expr: { $gte: [ { $size: "$poderes" }, 2 ] }
+          });
     }
 }
 export default new SuperHeroRepository();
